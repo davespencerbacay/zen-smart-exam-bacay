0b45c55b696cdfaf3d45163bc6591f4860ae0fce
= <spencerbacay@gmail.com>
2022-06-21 06:10:07 +0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// pages
+import HomePage from "./pages/HomePage";
 
 const App = (props) => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,64 @@
+const colours = [
+  {
+    background: [
+      {
+        name: "Default",
+        color: "#e9edf9",
+      },
+      {
+        name: "Info",
+        color: "#dbe9fd",
+      },
+      {
+        name: "Success",
+        color: "#e0f6f2",
+      },
+      {
+        name: "Failure",
+        color: "#fce4e5",
+      },
+    ],
+  },
+  {
+    text: [
+      {
+        name: "Headings",
+        color: "#2e384d",
+      },
+      {
+        name: "Body",
+        color: "#8798ad",
+      },
+      {
+        name: "Unselected Nav",
+        color: "#b0bac9",
+      },
+      {
+        name: "Selected Nav",
+        color: "#194787",
+      },
+    ],
+  },
+  {
+    swatches: [
+      "#0b6bf2",
+      "#0243eb",
+      "#dbe9fd",
+      "#00c1d4",
+      "#00a6c0",
+      "#d9f6f9",
+      "#2cc2a5",
+      "#11a783",
+      "#e0f6f2",
+      "#f7c137",
+      "#f3a618",
+      "#fef6e1",
+      "#e84a50",
+      "#dc262b",
+      "#fce4e5",
+      "#8c54ff",
+      "#652eff",
+      "#eee6ff",
+    ],
+  },
+];
